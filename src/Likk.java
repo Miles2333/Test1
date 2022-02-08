d32252b4d59c97d6e40a595721b2a7188806edea
Miles <1978176436@qq.com>
2022-02-08 23:10:30 +0800

    IDEA整合Github测试

---
+++ src/Likk.java
@@ -0,0 +1,12 @@
+public class Likk {
+
+    public static void main(String[] args) {
+        int arr;
+
+    }
+
+
+
+
+
+}
---
+++ src/Node.java
@@ -0,0 +1,7 @@
+public class Node {
+
+    public static void main(String[] args) {
+        System.out.println("Dearjing");
+    }
+
+}
